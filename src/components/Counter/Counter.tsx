28d76603cb87fc39b25babe4fcbe1c4e2f0a5163
Cheryl Lee <clee0937@gmail.com>
2024-03-25 23:44:14 +0000

    added useState hook and increment and decrement functions for +/- buttons

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,15 +1,34 @@
 import "./Counter.scss";
+import { useState } from "react";
 
 const Counter = () => {
+  const [counter, setCounter] = useState<number>(0);
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
+
+  const handleDecrement = () => {
+    if (counter >= 1) {
+      setCounter(counter - 1);
+    }
+  };
+
   return (
     <div className="counter">
       <p className="counter__items counter__items--heading">Counter</p>
-      <p className="counter__items counter__items--value">0</p>
+      <p className="counter__items counter__items--value">{counter}</p>
       <div className="counter__button-container">
-        <button className="counter__button counter__button--increment">
+        <button
+          className="counter__button counter__button--increment"
+          onClick={handleIncrement}
+        >
           +
         </button>
-        <button className="counter__button counter__button--decrement">
+        <button
+          className="counter__button counter__button--decrement"
+          onClick={handleDecrement}
+        >
           -
         </button>
       </div>
