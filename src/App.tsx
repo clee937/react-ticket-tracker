e2035021ba7bfd0b6b7e3a86e76786c6cf2bd9bf
Cheryl Lee <clee0937@gmail.com>
2024-03-25 23:10:59 +0000

    mapped over data file to create EmployeeCard components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import "./App.scss";
-import CardContainer from "./components/CardContainer/CardContainer";
+import EmployeeCard from "./components/EmployeeCard/EmployeeCard";
+import team from "./data/team";
 
 function App() {
   return (
     <div className="app">
       <h1 className="app__title">Ticket Tracker</h1>
-      <CardContainer />
+      <div className="app__container">
+        {team.map((employee) => (
+          <EmployeeCard
+            key={employee.id}
+            name={employee.name}
+            role={employee.role}
+          />
+        ))}
+      </div>
     </div>
   );
 }
