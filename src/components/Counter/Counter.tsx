e3e5400c6a86d4195bdabc9670bb1df6654f460f
Cheryl Lee <clee0937@gmail.com>
2024-03-28 06:47:19 +0000

    updated styling on buttons

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -20,16 +20,17 @@ const Counter = () => {
       <p className="counter__items counter__items--value">{counter}</p>
       <div className="counter__button-container">
         <button
-          className="counter__button counter__button--increment"
-          onClick={handleIncrement}
-        >
-          +
-        </button>
-        <button
           className="counter__button counter__button--decrement"
           onClick={handleDecrement}
         >
-          -
+          &minus;
+        </button>
+
+        <button
+          className="counter__button counter__button--increment"
+          onClick={handleIncrement}
+        >
+          &#43;
         </button>
       </div>
     </div>
